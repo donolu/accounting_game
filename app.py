ec209bc37f235f76479a5e794c50653869ecf3ad
Olumide Ibilaiye <olumideibilaiye@gmail.com>
2025-02-01 23:33:25 +0000

    Updated credentials loading

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import json
 try:
     creds_str = st.secrets["GOOGLE_SHEETS_CREDENTIALS"]
     st.write("✅ Successfully loaded secrets.")
-    st.write("First 200 characters of credentials:", creds_str[:200])
+    st.write("First 400 characters of credentials:", creds_str[:400])
 except Exception as e:
     st.error(f"❌ Error loading secrets: {e}")
